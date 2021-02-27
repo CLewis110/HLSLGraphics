25d3066d2d5253cd246d0466d819acadfbfb615e
Lewicv83 <lewicv83@gmail.com>
2021-02-27 11:38:07 -0500

    Create Window

--- WinMain.cpp
+++ WinMain.cpp
@@ -6,6 +6,32 @@ int CALLBACK WinMain(
 	LPSTR	lpCmdLine,
 	int		nCmdShow)
 {
+	const auto pClassName = "hw3d";
+	//register window class
+	WNDCLASSEX wc = {0};
+	wc.cbSize = sizeof( wc );
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = nullptr;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+	RegisterClassEx( &wc );
+
+	//create window
+	HWND hWnd = CreateWindowEx(
+		0, pClassName,
+		"The Window of Windows",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200, 200, 640, 480,
+		nullptr, nullptr, hInstance, nullptr
+	);
+
 	while (true);
 	return 0;
 }
