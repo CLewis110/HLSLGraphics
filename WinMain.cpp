8804adf05f36b7fe33312917f9227aa4deddb4ce
Lewicv83 <lewicv83@gmail.com>
2021-02-27 17:40:02 -0500

    Added Window Class

--- WinMain.cpp
+++ WinMain.cpp
@@ -1,48 +1,5 @@
-#include <Windows.h>
-#include "WindowsMessageMap.h"
-#include <sstream>
+#include "Window.h"
 
-//Close program if window is closed
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-	static WindowsMessageMap mp;
-	OutputDebugString(mp(msg, lParam, wParam).c_str());
-
-	switch (msg)
-	{
-	case WM_CLOSE:
-		PostQuitMessage(123);
-		break;
-	case WM_KEYDOWN:
-		if (wParam == 'D')
-		{
-			SetWindowText(hWnd, "New Window Title");
-		}
-		break;
-	case WM_KEYUP:
-		if ((wParam == 'F'))
-		{
-			SetWindowText(hWnd, "Old Window Title");
-		}
-		break;
-	case WM_CHAR:
-		{
-			static std::string title;
-			title.push_back((char)wParam);
-			SetWindowText(hWnd, title.c_str());
-		}
-		break;
-	case WM_LBUTTONDOWN:
-		{
-			POINTS pt = MAKEPOINTS(lParam);
-			std::ostringstream oss;
-			oss << "(" << pt.x << ", " << pt.y << ")";
-			SetWindowText(hWnd, oss.str().c_str());
-		}
-		break;
-	}
-	return DefWindowProc(hWnd, msg, wParam, lParam);
-}
 
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
@@ -50,35 +7,7 @@ int CALLBACK WinMain(
 	LPSTR	lpCmdLine,
 	int		nCmdShow)
 {
-	const auto pClassName = "hw3d";
-
-	//register window class
-	WNDCLASSEX wc = {0};
-	wc.cbSize = sizeof( wc );
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = nullptr;
-	wc.hIcon = nullptr;
-	wc.hCursor = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = pClassName;
-	wc.hIconSm = nullptr;
-	RegisterClassEx( &wc );
-
-	//create window
-	HWND hWnd = CreateWindowEx(
-		0, pClassName,
-		"The Window of Windows",
-		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-		200, 200, 640, 480,
-		nullptr, nullptr, hInstance, nullptr
-	);
-
-	//Show window
-	ShowWindow(hWnd, SW_SHOW);
+	Window wnd(800, 300, "My Window");
 
 	//Message handling
 	MSG msg;
@@ -89,15 +18,11 @@ int CALLBACK WinMain(
 		DispatchMessage(&msg);
 	}
 
-
 	if (gResult == -1)
 	{
 		return -1;
 	}
-	else
-	{
-		return msg.wParam;
-	}
 
-	return 0;
+	return msg.wParam;
+
 }
